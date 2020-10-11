b8999d08f3edbdf5b7694a9dc6613e507b66f850
James Whale <james@james-whale.com>
2020-10-11 18:02:23 +0100

    allowed for SQL queries to be tracked in flask_debugtoolbar

--- personal_website/config.py
+++ personal_website/config.py
@@ -6,6 +6,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://james:whale@localhost/jameswhale'
+    SQLALCHEMY_RECORD_QUERIES = True
     DEBUG = True
 
 
