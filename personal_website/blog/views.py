206bd34c4e4814ec2893ab929ce6746d9900cd80
James Whale <james@james-whale.com>
2020-10-17 16:57:58 +0100

    added last_updated section and fixed implementaion

--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -9,10 +9,10 @@ from personal_website.models import Blogs
 @blog.route('/')
 def blog_homepage():
     blogs = Blogs.query.filter(Blogs.hidden == False).order_by(desc(Blogs.start_date)).all()
-    return render_template('blog.html', blogs=blogs)
+    return render_template('blog.html', blogs=blogs, last_updated='2020-10-17')
 
 
 @blog.route('/<link>')
 def blog_post(link):
     post = Blogs.query.filter_by(link=link).first_or_404()
-    return render_template('post.html', blog=post)
+    return render_template('post.html', blog=post, last_updated=post.last_updated)
--- personal_website/main/views.py
+++ personal_website/main/views.py
@@ -12,12 +12,12 @@ from .forms import ContactForm
 def index():
     recent = Blogs.query.filter(Blogs.hidden == False).order_by(Blogs.created.desc()).limit(5)
     latest = Blogs.query.filter(Blogs.hidden == False).order_by(Blogs.created.desc()).first()
-    return render_template('index.html', recent=recent, latest=latest)
+    return render_template('index.html', recent=recent, latest=latest, last_updated='2019-07-08')
 
 
 @main.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', last_updated='2019-04-19')
 
 
 @main.route('/contact', methods=['GET', 'POST'])
@@ -30,14 +30,14 @@ def contact():
         flash('Name: {}, Email: {}, Message: {} | SENT'.format(name, email, message))
         return redirect(url_for('index'))
     flash('WARNING: This contact form does not work. Any information input will be lost.')
-    return render_template('contact.html', form=form)
+    return render_template('contact.html', form=form, last_updated='2019-07-04')
 
 
 @main.route('/privacy_policy')
 def privacy_policy():
-    return render_template('privacy_policy.html')
+    return render_template('privacy_policy.html', last_updated='2019-04-22')
 
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', last_updated='2020-10-17'), 404
