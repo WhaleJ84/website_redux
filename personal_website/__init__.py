0017211834766cfb35d9cdcf80bbd86c9afb2c0c
James Whale <james@james-whale.com>
2020-11-28 18:53:37 +0000

    disabled toolbar

--- personal_website/__init__.py
+++ personal_website/__init__.py
@@ -1,7 +1,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 from .config import config_by_name
 
@@ -12,7 +12,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
-    toolbar = DebugToolbarExtension(app)
+    # toolbar = DebugToolbarExtension(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
