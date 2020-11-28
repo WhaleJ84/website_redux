dfe8034c11b50e9c0cc3592c9acba38c29399e5a
James Whale <james@james-whale.com>
2020-11-28 18:39:03 +0000

    fixed typo

--- build/scripts/010-server_installs.sh
+++ build/scripts/010-server_installs.sh
@@ -5,7 +5,7 @@ apt update
 apt upgrade -y
 
 # install necessary system packages
-apt apt install -y build-essential libssl-dev libffi-dev snapd nginx
+apt install -y build-essential libssl-dev libffi-dev snapd nginx
 
 # install necessary python packages
 apt install -y python3-pip python3-dev python3-setuptools python3-venv python3-certbot-nginx
