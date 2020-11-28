04ca4920363d00c3390e5a7384be22532e15572e
James Whale <james@james-whale.com>
2020-11-28 18:47:07 +0000

    changed interpreter to bash to fix source issue

--- build/scripts/020-pip.sh
+++ build/scripts/020-pip.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+# NOTE: this is the only script with the bash interpreter
+# This is because the `source` command is not defined in POSIX shell
+# If anybody knows how I would do the same in shell, please let me know!
 
 # ensure in correct working directory
 cd /var/www/website_redux/ || return
