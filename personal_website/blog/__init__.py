1a69faad4aa2d7ec5a8f8dbf8a820ef33f111ff4
James Whale <james@james-whale.com>
2020-11-28 21:45:12 +0000

    fixed linting warnings

--- personal_website/blog/__init__.py
+++ personal_website/blog/__init__.py
@@ -1,5 +1,9 @@
+"""
+Initialises the blog blueprint used while building app.
+"""
 from flask import Blueprint
 
 blog = Blueprint('blog', __name__)
 
-from . import views
+# imports have to remain down here to prevent circular import errors
+from . import views  # pylint: disable=wrong-import-position
--- personal_website/main/views.py
+++ personal_website/main/views.py
@@ -1,8 +1,12 @@
+"""
+Contains all the routes (views) needed for the main blueprint.
+Things such as functions and forms should be in separate files.
+"""
 
 from flask import render_template, redirect, url_for, flash
 
-from . import main
 from personal_website.models import Blogs
+from . import main
 from .forms import ContactForm
 
 
@@ -10,6 +14,11 @@ from .forms import ContactForm
 @main.route('/index')
 @main.route('/home')
 def index():
+    # pylint: disable=singleton-comparison
+    """
+    Directs to the index (homepage).
+    :return:
+    """
     recent = Blogs.query.filter(Blogs.hidden == False).order_by(Blogs.created.desc()).limit(5)
     latest = Blogs.query.filter(Blogs.hidden == False).order_by(Blogs.created.desc()).first()
     return render_template('index.html', recent=recent, latest=latest, last_updated='2019-07-08')
@@ -17,11 +26,19 @@ def index():
 
 @main.route('/about')
 def about():
+    """
+    Directs to the about-me page.
+    :return:
+    """
     return render_template('about.html', last_updated='2019-04-19')
 
 
 @main.route('/contact', methods=['GET', 'POST'])
 def contact():
+    """
+    Directs to the contact page.
+    :return:
+    """
     form = ContactForm()
     if form.validate_on_submit():
         name = form.name.data
@@ -35,9 +52,19 @@ def contact():
 
 @main.route('/privacy_policy')
 def privacy_policy():
+    """
+    Directs to the privacy policy page.
+    :return:
+    """
     return render_template('privacy_policy.html', last_updated='2019-04-22')
 
 
 @main.app_errorhandler(404)
-def page_not_found(e):
+def page_not_found(error):
+    """
+    Redirects to the 404 error page.
+    :param error:
+    :return:
+    """
+    print(error)
     return render_template('404.html', last_updated='2020-10-17'), 404
