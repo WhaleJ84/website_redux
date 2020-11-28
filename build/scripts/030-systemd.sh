605f9d883873aef9cab2d4c8d5e2d9660e0ca24e
James Whale <james@james-whale.com>
2020-11-28 15:38:23 +0000

    renamed file appropriately

--- build/scripts/030-systemd.sh
+++ build/scripts/030-systemd.sh
@@ -6,4 +6,3 @@ cp ../website/jameswhale.service /etc/systemd/system/jameswhale.service
 # confirm service is enabled and running
 systemctl start jameswhale
 systemctl enable jameswhale
-systemctl status jameswhale
